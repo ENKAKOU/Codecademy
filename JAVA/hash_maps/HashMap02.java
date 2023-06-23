8fcacbb7a23b6eb64a169ab0ac98f3ea81791330
enkakou <enkakou0716@gmail.com>
2023-06-23 14:39:05 +0900

    hash_maps

---
+++ JAVA/hash_maps/HashMap02.java
@@ -0,0 +1,12 @@
+public class HashMap02 {
+
+    public String[] hashmap;
+
+    public HashMap(int size) {
+        this.hashmap = new String[size];
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
