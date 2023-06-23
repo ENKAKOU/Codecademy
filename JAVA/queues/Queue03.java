736e06f3b0347cd1f729859eea15050cfcbe8f73
enkakou <enkakou0716@gmail.com>
2023-06-23 11:54:22 +0900

    queues

---
+++ JAVA/queues/Queue03.java
@@ -0,0 +1,28 @@
+public class Queue03 {
+
+    public LinkedList queue;
+    public int size;
+    static final int DEFAULT_MAX_SIZE = Integer.MAX_VALUE;
+    public int maxSize;
+
+    public Queue() {
+        this(DEFAULT_MAX_SIZE);
+    }
+
+    public Queue(int maxSize){
+        this.queue = new LinkedList();
+        this.size = 0;
+        this.maxSize = maxSize;
+    }
+
+    // Define enqueue() below
+    public static void main(String[] args) {
+
+        Queue coffeeOrder = new Queue();
+        System.out.println("coffeeOrder queue has " + coffeeOrder.size + " orders.");
+        coffeeOrder.enqueue("latte");
+        coffeeOrder.enqueue("espresso");
+        coffeeOrder.enqueue("cappuccino");
+
+    }
+}
