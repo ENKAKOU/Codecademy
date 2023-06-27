b124d279f8fdc490ddedf0bc249f27e478cc4a6c
enkakou <enkakou0716@gmail.com>
2023-06-27 15:39:47 +0900

    BubbleSort

---
+++ JAVA/Sorting_Algorithms/BubbleSort/BubbleSort.java
@@ -0,0 +1,14 @@
+public class BubbleSort {
+
+    public static int[] bubbleSort(int input[]) {
+        boolean swapping = true;
+        while (swapping) {
+            swapping = false;
+            for (int i = 0; i < input.length - 1; i++) {
+
+            }
+        }
+        return input;
+
+    }
+}
