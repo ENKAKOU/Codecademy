6ee0004830183671702e3503f4d31b609767ee0d
enkakou <enkakou0716@gmail.com>
2023-06-27 12:11:09 +0900

    MinHeap07

--- JAVA/Heaps/MinHeap07.java
+++ JAVA/Heaps/MinHeap07.java
@@ -23,7 +23,8 @@ public class MinHeap07 {
         if (this.size == 0) {
             throw new Error("Heap is empty!");
         }
-        System.out.println("Swap min element " + this.heap.get(1) + " and last element " + this.heap.get(this.size));
+        System.out.println("Swap min element " + this.heap.get(1) +
+                " and last element " + this.heap.get(this.size));
         this.swap(1, this.size);
         int min = this.heap.remove(this.size);
         System.out.println("Removed from the heap: " + min);
@@ -34,8 +35,10 @@ public class MinHeap07 {
 
     private void bubbleUp() {
         int current = this.size;
-        while (current > 1 && this.heap.get(current) < this.heap.get(this.getParent(current))) {
-            System.out.println("Swap index " + current + " with index " + this.getParent(current));
+        while (current > 1 && this.heap.get(current) <
+                this.heap.get(this.getParent(current))) {
+            System.out.println("Swap index " + current +
+                    " with index " + this.getParent(current));
             System.out.println(this.heap);
             this.swap(current, this.getParent(current));
             current = this.getParent(current);
@@ -66,8 +69,10 @@ public class MinHeap07 {
     }
 
     private boolean canSwap(int current, int leftChild, int rightChild) {
-        return (this.exists(leftChild) && (this.heap.get(current) > this.heap.get(leftChild)))
-                || (this.exists(rightChild) && (this.heap.get(current) > this.heap.get(rightChild)));
+        return (this.exists(leftChild) && (this.heap.get(current) >
+                this.heap.get(leftChild)))
+                || (this.exists(rightChild) && (this.heap.get(current) >
+                this.heap.get(rightChild)));
     }
 
     public int getParent(int current) {
