acc261fbc786935062a6c676ad65ab94cad50f2a
enkakou <enkakou0716@gmail.com>
2023-06-28 14:56:59 +0900

    serialization

--- JAVA/serialization/Car05.java
+++ JAVA/serialization/Car05.java
@@ -6,24 +6,28 @@ import java.io.ObjectInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-public class Car03 implements Serializable {
+public class Car05 implements Serializable {
     private String make;
     private int year;
+    private Engine engine;
     private static final long serialVersionUID = 1L;
 
     public Car(String make, int year) {
         this.make = make;
         this.year = year;
+        this.engine = new Engine(2.4, 6);
     }
 
-    public String toString(){
-        return String.format("Car make is: %s, Car year is: %d", this.make, this.year);
+    public String toString() {
+        return String.format("Car make is: %s, Car year is: %d, " +
+                "%s", this.make, this.year, this.engine);
     }
 
     public static void main(String[] args) throws FileNotFoundException,
             IOException, ClassNotFoundException {
         Car toyota = new Car("Toyota", 2021);
         Car honda = new Car("Honda", 2020);
+
         FileOutputStream fileOutputStream = new FileOutputStream("cars.txt");
         ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
         objectOutputStream.writeObject(toyota);
@@ -39,6 +43,6 @@ public class Car03 implements Serializable {
         System.out.println("Toyota (Copy) - "+toyotaCopy);
         System.out.println("Toyota (Original) - "+toyota);
         System.out.println("Is same object: "+ isSameObject);
-
+        
     }
 }
