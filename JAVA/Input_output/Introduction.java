b5efb04050e0c388ecbbf4471d683f30907b3232
enkakou <enkakou0716@gmail.com>
2023-06-28 13:44:49 +0900

    Input_output

---
+++ JAVA/Input_output/Introduction.java
@@ -0,0 +1,14 @@
+public class Introduction {
+
+    public static void main(String[] args) {
+
+        String name = "yanzai";
+        String hobbies = "zhuifan";
+        int age = 19;
+
+        System.out.println("My name is " + name);
+        System.out.printf("I am %d years old.\n", age);
+        System.out.print("My hobbies are " + hobbies);
+
+    }
+}
