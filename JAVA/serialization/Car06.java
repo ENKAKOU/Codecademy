6a038fada7e5647bd7a3201cc1b8db39b050a24b
enkakou <enkakou0716@gmail.com>
2023-06-28 15:02:56 +0900

    serialization

--- JAVA/serialization/Car06.java
+++ JAVA/serialization/Car06.java
@@ -6,11 +6,11 @@ import java.io.ObjectInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-public class Car05 implements Serializable {
+public class Car06 implements Serializable {
     private String make;
     private int year;
-    private Engine engine;
     private static final long serialVersionUID = 1L;
+    private Engine engine;
 
     public Car(String make, int year) {
         this.make = make;
@@ -18,7 +18,23 @@ public class Car05 implements Serializable {
         this.engine = new Engine(2.4, 6);
     }
 
-    public String toString() {
+    private void writeObject(ObjectOutputStream stream) throws IOException {
+        stream.writeObject(this.make);
+        stream.writeInt(this.year);
+        stream.writeDouble(this.engine.getLiters());
+        stream.writeInt(this.engine.getCylinders());
+    }
+
+    private void readObject(ObjectInputStream stream) throws
+            IOException, ClassNotFoundException {
+        this.make = (String) stream.readObject();
+        this.year = (int) stream.readInt();
+        double liters = (double) stream.readDouble();
+        int cylinders = (int) stream.readInt();
+        this.engine = new Engine(liters, cylinders);
+    }
+
+    public String toString(){
         return String.format("Car make is: %s, Car year is: %d, " +
                 "%s", this.make, this.year, this.engine);
     }
@@ -27,7 +43,7 @@ public class Car05 implements Serializable {
             IOException, ClassNotFoundException {
         Car toyota = new Car("Toyota", 2021);
         Car honda = new Car("Honda", 2020);
-
+        
         FileOutputStream fileOutputStream = new FileOutputStream("cars.txt");
         ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
         objectOutputStream.writeObject(toyota);
