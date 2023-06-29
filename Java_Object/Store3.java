e84a316135e878f407a1362d293f6b1e14d79050
enkakou <enkakou0716@gmail.com>
2023-06-29 23:46:29 +0900

    Java_Object

---
+++ Java_Object/Store3.java
@@ -0,0 +1,13 @@
+public class Store03 {
+
+    public Store03() {
+        System.out.println("I am inside the constructor method.");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Start of the main method.");
+
+        Store3 lemonadeStand = new Store3();
+        System.out.println(lemonadeStand);
+    }
+}
