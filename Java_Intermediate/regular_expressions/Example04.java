9e77aa0dfcbbc0b57c51e6db864ed14f06b101a3
enkakou <enkakou0716@gmail.com>
2023-06-30 13:59:54 +0900

    regular_expressions

---
+++ Java_Intermediate/regular_expressions/Example04.java
@@ -0,0 +1,13 @@
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class Example04 {
+
+    public static void main(String[] args){
+        String[] strings = {"CCQQ", "QQCC", "CQQ", "QUACK", "CCQ", "CQC", "CQQQ", "CCCQQ"};
+
+        for(String s: strings){
+
+        }
+    }
+}
