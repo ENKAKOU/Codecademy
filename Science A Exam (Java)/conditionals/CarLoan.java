6ccde13284d0ebc5be3f4e12d6e43b33b657158e
enkakou <enkakou0716@gmail.com>
2023-07-03 13:52:20 +0900

    Science A Exam (Java) - conditionals

---
+++ Science A Exam (Java)/conditionals/CarLoan.java
@@ -0,0 +1,6 @@
+public class CarLoan {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Science A Exam (Java)/conditionals/Operators.java
@@ -0,0 +1,13 @@
+public class Operators {
+
+    public static void main(String[] args) {
+        int a = 6;
+        int b = 3;
+
+        boolean ex1 = !(a == 7 && (b >= a || a != a));
+        boolean ex2 = a == b || !(b > 3);
+        boolean ex3 = !(b <= a && b != a + b);
+//         System.out.println(ex3);
+
+    }
+}
