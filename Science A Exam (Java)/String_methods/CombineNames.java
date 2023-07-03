842b0e58a45e523e3d9ceb5e788f614a3eaf4464
enkakou <enkakou0716@gmail.com>
2023-07-03 14:03:16 +0900

    Science A Exam (Java) - String_methods

---
+++ Science A Exam (Java)/String_methods/CombineNames.java
@@ -0,0 +1,10 @@
+public class CombineNames {
+
+    public static void main(String[] args) {
+        String firstName = "Ziggy";
+        String lastName = "Stardust";
+
+        System.out.println(firstName.concat(" " + lastName));
+
+    }
+}
---
+++ Science A Exam (Java)/String_methods/Password.java
@@ -0,0 +1,9 @@
+public class Password {
+
+    public static void main(String[] args) {
+        String password = "correcthorsebatterystaple";
+
+        System.out.println(password.equals("Tr0ub4dor&3"));
+
+    }
+}
