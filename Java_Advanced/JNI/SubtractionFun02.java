3f91c44b3a898ebac9acaeb4e37472d04e87f7d0
enkakou <enkakou0716@gmail.com>
2023-07-03 13:50:18 +0900

    Java_Advanced / JNI

---
+++ Java_Advanced/JNI/SubtractionFun02.java
@@ -0,0 +1,14 @@
+public class SubtractionFun02 {
+    static {
+        System.loadLibrary("native");
+    }
+
+    private native int subtractValues(double num1, double num2);
+
+    public static void main(String[] args) {
+        double num1 = 1;
+        double num2 = 2;
+        int difference = new SubtractionFun().subtractValues(num1, num2);
+
+    }
+}
