ee7627d5e3135bd2b585cd8e10d5381622673d13
enkakou <enkakou0716@gmail.com>
2023-07-05 12:54:43 +0900

    Java (primary) - arrays_arraylists

--- Java (primary)/arrays_arraylists/arraylists/Shopping.java
+++ Java (primary)/arrays_arraylists/arraylists/Shopping.java
@@ -7,10 +7,8 @@ class Shopping {
         String[] groceryItems = {"steak", "milk", "jelly beans"};
         double[] prices = {25.00, 2.95, 2.50};
 
-        // Adding ham to the groceries
         groceryItems[3] = "ham";
         prices[3] = 4.99;
 
     }
-
 }
