3f984389f571757cedee3a466e9da772a3fbb255
enkakou <enkakou0716@gmail.com>
2023-07-05 13:00:36 +0900

    Java_Science A Exam - Arrays_ArrayLists

---
+++ Java_Science A Exam/Arrays_ArrayLists/Newsfeed2.java
@@ -0,0 +1,18 @@
+public class Newsfeed2 {
+
+        public Newsfeed2(){
+
+        }
+
+        public String[] getTopics(){
+            String[] topics = {"Opinion", "Tech", "Science", "Health"};
+            return topics;
+        }
+
+        public static void main(String[] args){
+            Newsfeed2 sampleFeed = new Newsfeed2();
+            String[] topics = sampleFeed.getTopics();
+            System.out.println(topics);
+
+        }
+    }
---
+++ Java_Science A Exam/Arrays_ArrayLists/ToDos2.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+class ToDos2 {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> toDoList = new ArrayList<String>();
+        String toDo1 = "Water plants";
+        String toDo2 = "Do laundry";
+        String toDo3 = "Pet Ham and Ghoti until they purr";
+
+        toDoList.add(toDo1);
+        toDoList.add(toDo2);
+        toDoList.add(toDo3);
+
+        System.out.println(toDoList);
+
+    }
+}
