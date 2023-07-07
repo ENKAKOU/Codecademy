e0165e5314480d4ec0dfe1bf0f66c674a4ed1f57
enkakou <enkakou0716@gmail.com>
2023-07-07 13:36:55 +0900

    Rest APIs & Spring Java - Java_I - HelloWorld

---
+++ Rest APIs & Spring Java/Java_I/HelloWorld/Tree.java
@@ -0,0 +1,14 @@
+public class Tree {
+
+    public static void main(String[] args) {
+
+        System.out.println("hi! I am Jack!");
+        System.out.println("I will plant a tree");
+        System.out.println("  *  ");
+        System.out.println(" *** ");
+        System.out.println(" *** ");
+        System.out.println("  *  ");
+        System.out.println("  *  ");
+
+    }
+}
