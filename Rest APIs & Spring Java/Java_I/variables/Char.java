01a4caa056e30316a3a3b515eeb3c2d5ae7afadf
enkakou <enkakou0716@gmail.com>
2023-07-07 14:03:29 +0900

    Rest APIs & Spring Java - Java_I - variables

---
+++ Rest APIs & Spring Java/Java_I/variables/Char.java
@@ -0,0 +1,11 @@
+public class Char {
+
+    public static void main(String[] args) {
+        char expectedGrade = 'A';
+        char expectedGrade = 'B';
+        char expectedGrade = 'C';
+        char expectedGrade = 'D';
+        char expectedGrade = 'F';
+
+    }
+}
---
+++ Rest APIs & Spring Java/Java_I/variables/MyProfile.java
@@ -0,0 +1,11 @@
+public class MyProfile {
+
+    public static void main(String[] args) {
+        String name = "ghj";
+        int age = 25;
+        double desirdeSalary = 60000000.00;
+        char gender = 'm';
+        boolean lookingForJob = true;
+
+    }
+}
