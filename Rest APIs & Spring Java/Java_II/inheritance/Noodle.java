db639904befc524e2418f90241977c186246a7f9
enkakou <enkakou0716@gmail.com>
2023-07-11 12:27:49 +0900

    Rest APIs & Spring Java - Java_II - inheritance

---
+++ Rest APIs & Spring Java/Java_II/inheritance/Noodle.java
@@ -0,0 +1,15 @@
+class Noodle {
+
+    double lengthInCentimeters;
+    String shape;
+    String texture = "brittle";
+
+    public void cook() {
+        this.texture = "cooked";
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
