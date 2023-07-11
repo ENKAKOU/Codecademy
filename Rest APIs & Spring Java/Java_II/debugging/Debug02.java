580691daa43476a65fd09f92c1f26d382901ca47
enkakou <enkakou0716@gmail.com>
2023-07-11 13:36:20 +0900

    Rest APIs & Spring Java - Java_II - debugging

---
+++ Rest APIs & Spring Java/Java_II/debugging/Debug02.java
@@ -0,0 +1,12 @@
+public class Debug02 {
+
+    public static void main(String[] args) {
+
+        int width = 20;
+        int length = 40;
+        int ratio = length / width;
+
+        System.out.println(ratio);
+
+    }
+}
---
+++ Rest APIs & Spring Java/Java_II/debugging/Debug03.java
@@ -0,0 +1,12 @@
+public class Debug03 {
+
+    public static void main(String[] args) {
+
+        int width = 0;
+        int length = 40;
+        int ratio = length / width;
+
+        System.out.println(ratio);
+
+    }
+}
