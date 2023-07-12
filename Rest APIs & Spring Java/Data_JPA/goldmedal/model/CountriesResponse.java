f0fa6074660563498f313704accff35d186f497e
enkakou <enkakou0716@gmail.com>
2023-07-12 14:24:41 +0900

    Rest APIs & Spring Java - goldmedal

---
+++ Rest APIs & Spring Java/Data_JPA/goldmedal/model/CountriesResponse.java
@@ -0,0 +1,16 @@
+package com.codecademy.goldmedal.model;
+
+import java.util.List;
+
+public class CountriesResponse {
+    private List<CountrySummary> countries;
+
+    public CountriesResponse(List<CountrySummary> countries) {
+        this.countries = countries;
+    }
+
+    public List<CountrySummary> getCountries() {
+        return countries;
+    }
+
+}
