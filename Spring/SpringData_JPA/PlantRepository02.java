d8ecab564b0847ad6b0f6c6d20e84397c3e15c31
enkakou <enkakou0716@gmail.com>
2023-07-18 11:43:09 +0900

    SpringData_JPA

---
+++ Spring/SpringData_JPA/PlantRepository02.java
@@ -0,0 +1,14 @@
+package com.codecademy.plants.repositories;
+
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+import com.codecademy.plants.entities.Plant;
+
+public interface PlantRepository02 extends CrudRepository<Plant, Integer> {
+    List<Plant> findByHasFruitTrue();
+    List<Plant> findByHasFruitFalse();
+    List<Plant> findByQuantityLessThan(Integer quantity);
+    List<Plant> findByHasFruitTrueAndQuantityLessThan(Integer quantity);
+    List<Plant> findByHasFruitFalseAndQuantityLessThan(Integer quantity);
+
+}
