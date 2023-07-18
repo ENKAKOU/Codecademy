7ec32dd28fcce6aa7e5097be0d114651802ce238
enkakou <enkakou0716@gmail.com>
2023-07-18 16:47:04 +0900

    Android_JavaFoundations

---
+++ Android Apps (Java)/Android_JavaFoundations/Tree.java
@@ -0,0 +1,13 @@
+public class Tree {
+
+    public static void main(String[] args){
+        System.out.println("Hey there, I’m Ariel and I’m learning to code in Java!");
+        System.out.println("  *  ");
+        System.out.println(" *** ");
+        System.out.println(" *** ");
+        System.out.println("  *  ");
+        System.out.println("  *  ");
+        System.out.println("  *  ");
+
+    }
+}
