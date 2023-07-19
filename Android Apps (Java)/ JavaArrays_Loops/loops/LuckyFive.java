0e8c5fd9f56d83d9c4f76416aed96eec6f284d0e
enkakou <enkakou0716@gmail.com>
2023-07-19 13:20:35 +0900

    loops

---
+++ Android Apps (Java)/ JavaArrays_Loops/loops/LuckyFive.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+class LuckyFive {
+
+    public static void main(String[] args) {
+
+        Random randomGenerator = new Random();
+        int dieRoll = randomGenerator.nextInt(6) + 1;
+
+    }
+}
