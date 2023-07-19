8d0cd13f0454da9ee3c797ac7f4847aebeec7a02
enkakou <enkakou0716@gmail.com>
2023-07-20 00:31:48 +0900

    loops

--- Java_Loops & Arrays/loops/Lunch.java
+++ Java_Loops & Arrays/loops/Lunch.java
@@ -19,18 +19,19 @@ class Lunch {
 //        i++;
 //      }
 //    }
-
         return lunchBox;
     }
 
     public static void main(String[] args) {
         ArrayList<String> lunchContainer = new ArrayList<String>();
+
         lunchContainer.add("apple");
         lunchContainer.add("ant");
         lunchContainer.add("ant");
         lunchContainer.add("sandwich");
         lunchContainer.add("ant");
         lunchContainer = removeAnts(lunchContainer);
+
         System.out.println(lunchContainer);
 
     }
