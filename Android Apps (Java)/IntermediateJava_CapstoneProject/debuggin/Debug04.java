ee850219c748ab9cf2dd0aede66ca1465f270542
enkakou <enkakou0716@gmail.com>
2023-07-20 15:24:43 +0900

    debuggin

---
+++ Android Apps (Java)/IntermediateJava_CapstoneProject/debuggin/Debug04.java
@@ -0,0 +1,11 @@
+public class Debug04 {
+
+    public static void main(String[] args) {
+        int steps = 10;
+        for (int i = 1; i <= steps; i++) {
+
+            System.out.println("Step #" + i);
+
+        }
+    }
+}
